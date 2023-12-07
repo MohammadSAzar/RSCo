70280053809cb296b267cf1d4d91094bb3ee7d11
Mohammad <mohazar9170@gmail.com>
2023-12-07 22:01:37 +0330

    cart_detail: completion of the template

--- cases/urls.py
+++ cases/urls.py
@@ -6,6 +6,7 @@ from . import views
 urlpatterns = [
     path('', views.CaseListView.as_view(), name='case_list'),
     path('add/<int:case_id>/', views.add_to_cart_view, name='cart_add'),
+    path('remove/<int:case_id>/', views.remove_from_cart_view, name='cart_remove'),
     path('detail/', views.cart_detail_view, name='cart_detail'),
     path('create/', views.order_create_view, name='order_create'),
     re_path(r'^case/(?P<slug>[-\w]+)/', views.CaseDetailView.as_view(), name='case_detail'),
